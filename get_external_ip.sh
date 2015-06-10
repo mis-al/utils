b01a186321b77e260d70dbca5ea1b28550988bf2
Александр <aleksandr.misyura@gmail.com>
2015-06-10 10:01:03 +0300

    utils:

--- get_external_ip.sh
+++ get_external_ip.sh
@@ -8,4 +8,4 @@ then
     sudo apt-get install curl -y
 fi
 reset
-alias myextip="curl -s 2ip.ru | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | head -n 1"
+echo -e "alias myextip=\"curl -s 2ip.ru | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | head -n 1\"" >> ~/.bashrc
